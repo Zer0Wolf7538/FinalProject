64444c40401ee23d8d53e8e715da21ba23c199f2
reeseb25 <baeddanr@gmail.com>
2024-05-03 13:01:27 -0400

    places

--- app/src/main/java/com/example/finalproject/PlaceFragment.kt
+++ app/src/main/java/com/example/finalproject/PlaceFragment.kt
@@ -3,9 +3,16 @@ package com.example.finalproject
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
+import androidx.lifecycle.Lifecycle
 import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.finalproject.databinding.FragmentPlaceBinding
 
 
@@ -36,6 +43,21 @@ class PlaceFragment : Fragment() {
         binding.japanButton.setOnClickListener{
             rootView.findNavController().navigate(R.id.action_placeFragment_to_japanFragment)
         }
+        val menuHost: MenuHost = requireActivity()
+
+        menuHost.addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.options_menu, menu)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                return NavigationUI.onNavDestinationSelected(
+                    menuItem,
+                    requireView().findNavController()
+                )
+            }
+        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
+
         return rootView
     }
 
